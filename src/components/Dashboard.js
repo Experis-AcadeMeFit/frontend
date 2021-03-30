36b09a6de31266939d30a7cad6f4383aa198b12b
Kristian Andersen <krilleandersen@gmail.com>
2021-03-30 17:10:52 +0200

    Fix: cleanup dashboard added week calendar'ish top bar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,16 @@
+import {useState}from "react";
+
+import {format} from "date-fns";
+import {startOfWeek} from "date-fns";
+import {addDays} from "date-fns"
+import {startOfMonth} from "date-fns"
+import {endOfMonth} from "date-fns"
+import {endOfWeek} from "date-fns"
+import {isSameDay} from "date-fns"
+import {isSameMonth} from "date-fns"
+import {addMonths} from "date-fns"
+import {subMonths} from "date-fns"
+
 import ProgramComponent from './programs/ProgramComponent'
 import WorkoutComponent from './workout/WorkoutComponent'
 
@@ -6,24 +19,44 @@ import WorkoutComponent from './workout/WorkoutComponent'
 import '../CSS/Dashboard.css'
 
 
-import ExcrecisesList from './exercises/ExercisesList'
+const Dashboard =() => {
+    
+    const [currentMonth,setCurrentMonth]=useState(new Date())
 
-const dashboard =(props) => {
+    const renderDays=()=> {
+        const dayFormat = "E";
+        const dateFormat = "d";
+        const days = [];
     
+        let startDay = startOfWeek(currentMonth);
+    console.log(startDay)
+        for (let i = 0; i < 7; i++) {
+            let day=format(addDays(startDay, i), dayFormat)
+            let date=format(new Date(), dateFormat)
+          days.push(
+            <div className="col col-center" key={i}>
+              {day}
+            </div>
+          );
+        }
     
+        return <div className="days row">{days}</div>;
+      }
 
     return(
         <div className="row">
+              {renderDays()}
+            <div className="column"></div>
             <div className="column">
             <ProgramComponent/>
             </div>
                 <div className="column">
             <WorkoutComponent/>
             </div>
-        
+            <div className="column"></div>
        </div>
 
     );
 }
 
-export default dashboard
+export default Dashboard
