6b9d461c7f6acb96117949ba1a6d66833671e5e4
Kristian Andersen <krilleandersen@gmail.com>
2021-04-06 09:36:55 +0200

    Added: real endpoint for login/signup

--- src/utills/APICalls.js
+++ src/utills/APICalls.js
@@ -1,6 +1,6 @@
 
 
-export const API_URL='https://expwefitkey.herokuapp.com/';
+export const API_URL='https://expwefit.herokuapp.com/'
 export const API_AUTHLOGIN ='api/v1/login'
 export const API_AUTHREGISTRE='api/v1/users'
 export const API_EXERCISES="api/v1/exercises"
